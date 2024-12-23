b2f172ca2b894574bf639c188d5de7a28c1c08b9
e.uludag <uludag.emre.tr@gmail.com>
2024-12-23 15:24:30 +0300

    Remove App

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,7 +1,7 @@
 export const projectsData = [
     {
         id: 1,
-        name: 'Cloud Cost Efficiency Analytics App',
+        name: 'Cloud Cost Efficiency Analytics',
         description: "Me and my team consists of 5 other developers develop the backend and dashboard of a cloud cost efficiency data from both Azure and AWS, creating alerts for high cost usages, detects anomalies and displays possible cost saving potentials in a dashboard application",
         tools: ['DBT', 'AWS S3', 'AWS Athena', 'AWS Lambda', 'AWS SQS', 'AWS Quicksight', 'SQL', 'Python', 'Terraform'],
         role: 'DataEngineeringTeamLead',
