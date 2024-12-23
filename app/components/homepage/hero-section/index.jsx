7aa8ba9aaf017007fd114c67c36619919eb661da
e.uludag <uludag.emre.tr@gmail.com>
2024-12-23 23:09:54 +0300

    Remove contact me.

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -91,12 +91,12 @@ function HeroSection() {
           </div>
 
           <div className="flex items-center gap-3">
-            <Link href="#contact" className="bg-gradient-to-r from-red-600 to-orange-300 p-[3px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
-              <button className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1 hover:gap-3">
-                <span>Contact me</span>
-                <RiContactsFill size={16} />
-              </button>
-            </Link>
+            {/*<Link href="#contact" className="bg-gradient-to-r from-red-600 to-orange-300 p-[3px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">*/}
+            {/*  <button className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1 hover:gap-3">*/}
+            {/*    <span>Contact me</span>*/}
+            {/*    <RiContactsFill size={16} />*/}
+            {/*  </button>*/}
+            {/*</Link>*/}
 
             <Link className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-red-600 to-orange-400 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold" role="button" target="_blank" href={personalData.resume}
             >
