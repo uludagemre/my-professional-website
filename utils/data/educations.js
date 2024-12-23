93dd5a76d2f99868d5d6a48ecb32270250533b80
e.uludag <uludag.emre.tr@gmail.com>
2024-12-23 15:17:00 +0300

    Update the title of education.

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,14 +2,14 @@ export const educations = [
 ,
   {
     id: 1,
-    title: "Masters Degree in Computer Science",
+    title: "Masters of Computer Science",
     duration: "2021 - 2022",
     institution: "Technical University of Munich",
     image: "tum",
   },
   {
     id: 2,
-    title: "Bachelors Degree in Computer Science",
+    title: "Bachelor of Computer Science",
     duration: "2014 - 2019",
     institution: "Koc University",
     image: "koc",
