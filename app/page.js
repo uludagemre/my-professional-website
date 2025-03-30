786036905e2b0e2e0e97bb0f14bdca7fc75656df
uludagemre <uludag.emre.tr@gmail.com>
2025-03-31 00:25:08 +0200

    Update designation to Data Architect in metadata and personal data
    Updated titles and descriptions to reflect the new designation of "Data Architect" instead of "Data Engineer" across app metadata and personal data files. This ensures consistency in branding and professional identity.

--- app/page.js
+++ app/page.js
@@ -34,7 +34,6 @@ export default async function Home() {
       <Customers />
       <Projects />
       <Education />
-      {/*<Blog blogs={blogs} />*/}
       <ContactSection />
     </div>
   )
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Emre Uludag",
-  profile: '/profile.jpg',
+  profile: '/ghibli.jpg',
   designation: "Data Architect",
   description: "My name is Emre Uludag, and I am an enthusiastic Data Architect with a robust background in backend engineering. I am a fast learner with a self-directed attitude, passionate about exploring new technologies and solving complex problems. My core skills lie in Big Data Technologies, and I specialize in using Python, Scala, SQL, and Terraform, with a particular interest in cloud environments. I firmly believe that the cloud represents the future of the IT world. Additionally, I am always eager to take on new challenges that align with my skills and interests.",
   email: 'uludag.emre.tr@gmail.com',
